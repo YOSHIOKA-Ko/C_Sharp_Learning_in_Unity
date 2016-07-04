fdedf2bf8ed6b312e8a514905c7dbd998f196b8e
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-05 02:39:36 +0900

    Show age to console view

--- Assets/Scripts/LearningVariables.cs
+++ Assets/Scripts/LearningVariables.cs
@@ -6,6 +6,11 @@ public class LearningVariables : MonoBehaviour {
     void Start () {
         // Show "Hello World" to console view
         Debug.Log("Hello World");
+
+        // Show age to console view
+        int age;
+        age = 30;
+        Debug.Log(age);
     }
 
     void Update () {
