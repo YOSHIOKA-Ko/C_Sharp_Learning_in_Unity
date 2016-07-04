9811fdc9aacf61977af929c462e2edae2b322084
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-05 02:25:41 +0900

    Show "Hello World" to console view

---
+++ Assets/Scripts/LearningVariables.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LearningVariables : MonoBehaviour {
+
+    void Start () {
+        // Show "Hello World" to console view
+        Debug.Log("Hello World");
+    }
+
+    void Update () {
+
+    }
+}
