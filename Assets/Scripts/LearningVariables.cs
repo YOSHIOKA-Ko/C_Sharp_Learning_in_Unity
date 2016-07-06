baa27d0b9b2e9fbf70c4c8d793f97e5969775ffb
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-06 23:38:01 +0900

    Assign height1 into height2, and Show height2 to concole view

--- Assets/Scripts/LearningVariables.cs
+++ Assets/Scripts/LearningVariables.cs
@@ -7,10 +7,16 @@ public class LearningVariables : MonoBehaviour {
         // Show "Hello World" to console view
         Debug.Log("Hello World");
 
-        // Show age to console view
+        // Show vavible in age to console view
         int age;
         age = 30;
         Debug.Log(age);
+
+        // Assign height1 into height2, and Show height2 to concole view
+        float height1 = 160.5f;
+        float height2;
+        height2 = height1;
+        Debug.Log(height2);
     }
 
     void Update () {
