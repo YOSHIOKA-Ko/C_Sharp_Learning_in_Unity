ff80d67f6b499861c657f8c28e231dd793f116a9
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-07 00:03:44 +0900

    Show a in string to console view

--- Assets/Scripts/LearningVariables.cs
+++ Assets/Scripts/LearningVariables.cs
@@ -12,11 +12,16 @@ public class LearningVariables : MonoBehaviour {
         age = 30;
         Debug.Log(age);
 
-        // Assign height1 into height2, and Show height2 to concole view
+        // Assign height1 into height2, and Show height2 to console view
         float height1 = 160.5f;
         float height2;
         height2 = height1;
         Debug.Log(height2);
+
+        // Show a in string to console view
+        string userName;
+        userName = "YOSHIOKA Ko57";
+        Debug.Log(userName);
     }
 
     void Update () {
