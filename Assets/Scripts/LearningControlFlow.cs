06877f754d4f1f2396850368b532aa87ee6c4f5c
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-10 17:36:08 +0900

    Add Conditional

---
+++ Assets/Scripts/LearningControlFlow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LearningControlFlow : MonoBehaviour {
+
+    // Conditional
+    void Start () {
+        int herbNum = 1;
+        if (herbNum == 1) {
+            Debug.Log("Repairs 50");
+        }
+    }
+}
