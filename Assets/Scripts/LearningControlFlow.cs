203d515b14bcea50b33ebc49355875c638485380
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-10 17:44:27 +0900

    Add Conditional of if and else

--- Assets/Scripts/LearningControlFlow.cs
+++ Assets/Scripts/LearningControlFlow.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class LearningControlFlow : MonoBehaviour {
 
-    // Conditional
     void Start () {
+        // Conditional of if only
         int herbNum = 1;
         if (herbNum == 1) {
             Debug.Log("Repairs 50");
+        }
+
+        // Conditional of if and else
+        int stamina = 200;
+        if (stamina >= 100) {
+            Debug.Log("Attack!");
+        } else {
+            Debug.Log("Defense!");
         }
     }
 }
