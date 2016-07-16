1eee72bd9c366a243d01337b6586b5a4f1f7aa02
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-16 16:45:27 +0900

    Add a method of say hallo

---
+++ Assets/Scripts/LearningMethods.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LearningMethods : MonoBehaviour {
+
+    void SayHello() {
+        Debug.Log("Hello");
+    }
+
+    void Start () {
+        SayHello();
+    }
+}
