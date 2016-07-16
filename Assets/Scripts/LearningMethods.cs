ccd4d98b05abd47d29bef5246d982841fd960396
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-16 17:06:32 +0900

    Using the Add method, find the answer

--- Assets/Scripts/LearningMethods.cs
+++ Assets/Scripts/LearningMethods.cs
@@ -11,8 +11,18 @@ public class LearningMethods : MonoBehaviour {
         Debug.Log("Hello" + name);
     }
 
+    int Add(int a, int b){
+        int c = a + b;
+        return c;
+    }
+
     void Start () {
         SayHello();
         CallName("Tracer");
+
+        // Using the Add method, find the answer
+        int answer;
+        answer = Add(2, 3);
+        Debug.Log(answer);
     }
 }
