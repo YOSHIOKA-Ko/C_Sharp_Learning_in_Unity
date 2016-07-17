9285618355fef4eecf70427782afcd1f3a413d9b
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-17 15:53:57 +0900

    Show a value the added vector

---
+++ Assets/Scripts/LearningVector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LearningVector : MonoBehaviour {
+
+    void Start () {
+        Vector2 playerPosition = new Vector2(3.0f, 4.0f);
+        playerPosition.x += 8.0f;
+        playerPosition.y += 5.0f;
+        Debug.Log(playerPosition);
+    }
+}
