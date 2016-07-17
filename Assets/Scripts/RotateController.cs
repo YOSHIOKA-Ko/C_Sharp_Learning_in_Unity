1401505903cfc692481af307bbff04b3dcaa67c3
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-17 17:20:32 +0900

    Rotate object on mouse click

---
+++ Assets/Scripts/RotateController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateController : MonoBehaviour {
+
+    float rotateSpeed = 0;
+
+    void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            this.rotateSpeed = 10;
+        }
+
+        transform.Rotate(0, this.rotateSpeed, 0);
+    }
+}
