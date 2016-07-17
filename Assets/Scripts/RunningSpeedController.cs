f64c258832cfbcfab193aa0eabb7df040865fb5a
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-17 21:17:27 +0900

    Be translate object with mouse click-down

---
+++ Assets/Scripts/RunningSpeedController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunningSpeedController : MonoBehaviour {
+
+    float runningSpeed = 0;
+
+    void Update () {
+        if(Input.GetMouseButtonDown(0)) {
+            this.runningSpeed = .1f;
+        }
+
+        transform.Translate(this.runningSpeed, 0, 0);
+
+        if(this.runningSpeed <= .01) {
+            this.runningSpeed = 0;
+        } else {
+            this.runningSpeed *= .98f;
+        }
+    }
+}
