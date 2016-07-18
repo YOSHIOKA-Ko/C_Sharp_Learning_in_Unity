bbead7e4620162c2deb4859f79b531471069611c
YOSHIOKA Ko <madonote.ko57@gmail.com>
2016-07-18 16:44:50 +0900

    Move runner with swipe

--- Assets/Scripts/RunningSpeedController.cs
+++ Assets/Scripts/RunningSpeedController.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class RunningSpeedController : MonoBehaviour {
 
     float runningSpeed = 0;
+    Vector3 swipeStartPositions;
 
     void Update () {
         transform.Translate(this.runningSpeed, 0, 0);
+
+        if(Input.GetMouseButtonDown(0)) {
+            this.swipeStartPositions = Input.mousePosition;
+        } else if (Input.GetMouseButtonUp(0)) {
+            Vector2 swipeEndPositions = Input.mousePosition;
+            float swipeLength = (swipeEndPositions.x - this.swipeStartPositions.x);
+
+            this.runningSpeed = swipeLength / 500;
+        }
 
         if(this.runningSpeed <= .01) {
             this.runningSpeed = 0;
